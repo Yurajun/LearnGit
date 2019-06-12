e2121a0c4b68875c5ebb0eacddf2f1552cba40ef
yura <yurajun@mail.ru>
2019-06-12 12:22:18 +0300

    added three

--- yura.js
+++ yura.js
@@ -3,6 +3,6 @@ console.log('js');
 console.log('new js home edition');
 console.log('new commit yura');
 
-console.log(' 1 ssh new change"');
+console.log('3ssh new change"');
 console.log('ssh"');
 console.log('ssh"');
