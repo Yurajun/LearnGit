3657b9771861d631e4297d3c679ea9db3810c65f
Yurajun <yurajun@mail.ru>
2019-06-12 10:44:48 +0300

    added closure to ar
    initial commit
    
    very best commit

--- vasya.js
+++ vasya.js
@@ -1,9 +1,15 @@
 const sh = console.log.bind(console);
 sh('vasya.js');
+const arr = [];
 for (let i = 0; i < 5; i++) {
-	(x => sh(x)).bind(null, i)();
+	(x => arr.push(x))(i);
 }
+sh(arr[2]);
 console.log('vasya.js');
 for (let i = 0; i < 5; i++) {
 	console.log(i);
 }
+
+console.log('initial commit');
+
+console.log('create new branch');
