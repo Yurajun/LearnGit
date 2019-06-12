02889300dff1f0d6b058f831407d25acd889f98e
Yurajun <yurajun@mail.ru>
2019-06-12 10:44:48 +0300

    added closure to arr

--- vasya.js
+++ vasya.js
@@ -1,8 +1,10 @@
 const sh = console.log.bind(console);
 sh('vasya.js');
+const arr = [];
 for (let i = 0; i < 5; i++) {
-	(x => sh(x)).bind(null, i)();
+	(x => arr.push(x))(i);
 }
+sh(arr[2]);
 console.log('vasya.js');
 for (let i = 0; i < 5; i++) {
 	console.log(i);
